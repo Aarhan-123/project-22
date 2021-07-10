e30cd4b525aba8684571b4c77e61c88ec359b771
Aarhan-123 <85384534+Aarhan-123@users.noreply.github.com>
2021-07-10 14:44:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,102 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var roof;
+var bob1;
+var bob2;
+var bob3;
+var bob4;
+var bob5;
+var rope1;
+var rope2;
+var rope3;
+var rope4;
+var rope5;
+
+
+
+function setup() {
+	createCanvas(800, 600);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var roof_options={
+		isStatic:true			
+	}
+
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
+    World.add(world,roof);
+ 
+ 
+    var bob_options={
+      restitution:0.95,
+	  isStatic:true
+	}
+
+	bob1 = Bodies.circle(300,350,40,bob_options);
+	World.add(world,bob1);
+   
+	bob2 = Bodies.circle(350,350,40,bob_options);
+	World.add(world,bob2);
+    
+	bob3 = Bodies.circle(400,350,40,bob_options);
+	World.add(world,bob3);
+
+	bob4 = Bodies.circle(450,350,40,bob_options);
+	World.add(world,bob4);
+
+    bob5 = Bodies.circle(500,350,40,bob_options);
+	World.add(world,bob5);
+
+    rope1 = new Rope(bob1,roof,-100,0);
+	rope2 = new Rope(bob2,roof,-55,0);
+	rope3 = new Rope(bob3,roof,-10,0);
+	rope4 = new Rope(bob4,roof,45,0);
+	rope5 = new Rope(bob5,roof,90,0);
+
+
+
+	Engine.run(engine);
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("#99004d");
+
+  rect(roof.position.x,roof.position.y,230,20);
+ 
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  
+  ellipse(bob1.position.x,bob1.position.y,40);
+  ellipse(bob2.position.x,bob2.position.y,40);
+  ellipse(bob3.position.x,bob3.position.y,40);
+  ellipse(bob4.position.x,bob4.position.y,40);
+  ellipse(bob5.position.x,bob5.position.y,40);
+
+
+
+ // drawSprites();
+}
+
+
+function keyPressed(){
+
+	if(keyCode === LEFT_ARROW){
+		
+      Matter.Body.applyForce(bob5,bob5.position,{x:-85,y:50});
+	}
+}
+ 
